bf35b9129548dba2e86531fb26ac18f1e706998c
travawork <travaworkjp@gmail.com>
2026-08-13 01:30:37 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
+document.querySelectorAll("[data-year]").forEach(e=>e.textContent=new Date().getFullYear());
+document.querySelectorAll(".menu-toggle").forEach(btn=>{
+  btn.addEventListener("click",()=>document.querySelector(".nav-links").classList.toggle("show"));
+});
 
