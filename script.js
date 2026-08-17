350ad4b31e53643888aee28d407243bdebd203f6
travawork <travaworkjp@gmail.com>
2026-08-17 20:22:39 +0800

    Rename script.js to js/script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.querySelectorAll("a[href^='#']").forEach(a=>a.addEventListener("click",e=>{const id=a.getAttribute("href");if(id&&id!="#"){const el=document.querySelector(id);if(el){e.preventDefault();el.scrollIntoView({behavior:"smooth"})}}}));
