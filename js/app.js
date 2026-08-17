3832713399f3c2eda3b3b05a0b78aa654a131ca4
travawork <travaworkjp@gmail.com>
2026-08-17 14:11:25 +0800

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll("[data-year]").forEach(e=>e.textContent=new Date().getFullYear());
-document.querySelectorAll(".menu-toggle").forEach(btn=>{
-  btn.addEventListener("click",()=>document.querySelector(".nav-links").classList.toggle("show"));
-});
-
